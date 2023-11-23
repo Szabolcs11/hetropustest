d8d7932763111e7355c96a7eb0c645a24e2d2162
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2023-11-23 21:28:48 +0100

    megy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 5004;
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.status(200).json({ success: true, message: "megy" });
+  return res.status(200).json({ success: true, message: "megyy" });
 });
 
 app.listen(port, () => {
